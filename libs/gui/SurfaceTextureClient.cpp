ef71949150cbaa5a31e648e2e080af144c08044f
Saurabh Shah <saurshah@codeaurora.org>
2012-07-21 18:49:55 -0700

    frameworks/native: Add support for external-only layers.
    Add support for external-only layers, used for dual-display
    or closed captions.
    Do not draw external-only layers.
    
    Change-Id: Ieeb945d8a01f16a61a40433aa388a7c7822dcc8a

--- libs/gui/SurfaceTextureClient.cpp
+++ libs/gui/SurfaceTextureClient.cpp
@@ -30,6 +30,9 @@
 
 #include <private/gui/ComposerService.h>
 #include <hardware/hwcomposer.h>
+#ifdef QCOMHW
+#include <gralloc_priv.h>
+#endif
 
 namespace android {
 
@@ -75,6 +78,7 @@ void SurfaceTextureClient::init() {
     mReqHeight = 0;
     mReqFormat = 0;
     mReqUsage = 0;
+    mReqExtUsage = 0;
     mTimestamp = NATIVE_WINDOW_TIMESTAMP_AUTO;
     mCrop.clear();
     mScalingMode = NATIVE_WINDOW_SCALING_MODE_FREEZE;
@@ -538,7 +542,27 @@ int SurfaceTextureClient::setUsage(uint32_t reqUsage)
 {
     ALOGV("SurfaceTextureClient::setUsage");
     Mutex::Autolock lock(mMutex);
-    mReqUsage = reqUsage;
+
+#ifdef QCOMHW
+    if (reqUsage & GRALLOC_USAGE_PRIVATE_EXTERNAL_ONLY) {
+        //Set explicitly, since reqUsage may have other values.
+        mReqExtUsage = GRALLOC_USAGE_PRIVATE_EXTERNAL_ONLY;
+        //This flag is never independent. Always an add-on to
+        //GRALLOC_USAGE_EXTERNAL_ONLY
+        if(reqUsage & GRALLOC_USAGE_PRIVATE_EXTERNAL_BLOCK) {
+            mReqExtUsage |= GRALLOC_USAGE_PRIVATE_EXTERNAL_BLOCK;
+        } else if(reqUsage & GRALLOC_USAGE_PRIVATE_EXTERNAL_CC) {
+            mReqExtUsage |= GRALLOC_USAGE_PRIVATE_EXTERNAL_CC;
+        }
+    }
+#endif
+
+    // For most cases mReqExtUsage will be 0.
+    // reqUsage could come from app or driver. When it comes from app
+    // and subsequently from driver, the latter ends up overwriting
+    // the existing values. We cache certain values in mReqExtUsage
+    // to avoid being overwritten.
+    mReqUsage = reqUsage | mReqExtUsage;
     return OK;
 }
 
